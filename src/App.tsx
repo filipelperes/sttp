d6ac162cf97e2b799372de1f08b55a48daceb94b
Filipe Lago Peres <filipelperes@gmail.com>
2026-06-17 13:13:35 -0300

    feat: integrate DateDisplay, Settings, and WelcomeTour into App root
    - Add DateDisplay component below Clock for current date
    - Add SettingsButton floating trigger (bottom-right)
    - Add WelcomeTour onboarding for first-time visitors
    - All components compose declaratively within the root layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import Clock from "@/components/Clock";
+import DateDisplay from "@/components/DateDisplay";
 import HandleCommandPalette from "@/CommandPalette/components/HandleCommandPalette";
 import HandleWindow from "@/components/HandleWindow";
+import SettingsButton from "@/features/Settings/components/SettingsButton";
+import WelcomeTour from "@/features/WelcomeTour/components/WelcomeTour";
 
 const App = () => (
   <div id="App" className="flex items-center justify-center flex-col relative">
     <Clock />
+    <DateDisplay />
     <HandleCommandPalette />
     <HandleWindow />
+    <SettingsButton />
+    <WelcomeTour />
   </div>
 );
-
 
 export default App;
